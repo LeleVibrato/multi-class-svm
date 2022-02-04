b02d082c654b79a2c2b665f2b462552f044249c7
sam255 <64822672+sam255@users.noreply.github.com>
2022-02-04 11:25:51 +0800

    实现了选择核函数的接口

--- nlsvm.py
+++ nlsvm.py
@@ -3,58 +3,36 @@
 # @作者 : sam255
 # @文件名 : nlsvm.py
 # @软件 : SVM多分类器
+# @协议 : MIT
 
 import numpy as np
 from scipy import sparse
 import osqp
 from functools import partial
-from typing import Callable
+from typing import Callable, Literal
 
 
-def poly_kernel(X1: np.ndarray, X2: np.ndarray, degree: int) -> float:
-    """
-    多项式核函数
-    :param X1: 第一个向量
-    :param X2: 第二个向量
-    :param degree: 多项式的次数
-    :return float: 两个向量的内积
-    """
-    return (1+np.dot(X1, X2))**degree
-
-
-def gaussian_kernel(X1: np.ndarray, X2: np.ndarray, sigma: float) -> float:
-    """
-    高斯核函数
-    :param X1: 第一个向量
-    :param X2: 第二个向量
-    :param sigma: 高斯核函数的sigma
-    :return float: 两个向量的内积
-    """
-    return np.exp(-np.linalg.norm(X1-X2)**2 / (2 * (sigma ** 2)))
-
-
-def nlsvm_solve(X: np.ndarray, y: np.ndarray, classes: tuple[str, str], cost: float, degree: int) -> Callable:
+def nlsvm_solve(X: np.ndarray, y: np.ndarray, classes: tuple[str, str], cost: float, kernel: Callable) -> Callable:
     """
     训练一个以多项式为核函数的SVM二分类器
     :param X: 训练数据的特征矩阵，n行，dim列，dim为数据维数，每一行为一个训练样本，每一列为一个特征
     :param y: 训练数据的标签矩阵，n行，1列，取值为-1或1
     :param classes: 长度为2的字符串元组，表示当前用于训练的二分类
     :param cost: SVM超参数，alpha_i的上界
-    :param degree: 多项式核函数的次数
+    :param kernel: 多项式核函数的类型
     :return closure: SVM二分类器
     """
     n = X.shape[0]  # 样本个数
 
     # 计算内积
-    inner_product = np.ones((n, n))
+    inprod = np.ones((n, n))
     for i in range(n):
         for j in range(i+1):
-            inner_product[i, j] = inner_product[j, i] = poly_kernel(X[j, :],
-                                                                    X[i, :], degree)
+            inprod[i, j] = inprod[j, i] = kernel(X[j, :], X[i, :])
 
     # 定义二次规划参数矩阵
     q = -np.ones(n)
-    P = 0.5 * y @ y.T * inner_product
+    P = 0.5 * y @ y.T * inprod
     P[np.diag_indices_from(P)] = 2 * P[np.diag_indices_from(P)]
     A = np.vstack((np.diag(np.ones(n)), y.T))
     lb = np.zeros(n+1)
@@ -74,7 +52,7 @@ def nlsvm_solve(X: np.ndarray, y: np.ndarray, classes: tuple[str, str], cost: fl
     # 计算 beta0（取平均值）
     beta0sum = 0.0
     for j in svs_ind:
-        beta0sum += 1.0 / y[j, 0] - sum(alpha[i] * y[i, 0] * inner_product[j, i]
+        beta0sum += 1.0 / y[j, 0] - sum(alpha[i] * y[i, 0] * inprod[j, i]
                                         for i in range(n))
     beta0 = beta0sum / len(svs_ind)  # 取平均值
 
@@ -85,7 +63,7 @@ def nlsvm_solve(X: np.ndarray, y: np.ndarray, classes: tuple[str, str], cost: fl
         :return str: 样本 x 的预测类别
         """
         result = beta0 + \
-            sum(alpha[i] * y[i, 0] * poly_kernel(x, X[i, :], degree)
+            sum(alpha[i] * y[i, 0] * kernel(x, X[i, :])
                 for i in range(n))
         if result >= 0.0:
             return classes[0]
@@ -110,7 +88,51 @@ def ovo(y: np.ndarray) -> list[tuple[str, str]]:
             for i in range(num_of_classes) for j in range(i+1, num_of_classes)]
 
 
-def nlsvm(X: np.ndarray, y: np.ndarray, cost:  float = 10.0, degree: int = 3) -> list[Callable]:
+def get_kernel(kernel: str, kargs: dict) -> Callable:
+    """
+    根据参数生成核函数
+    :param kernel: 核函数类型
+    :param kargs: 核函数超参数
+    :return Callable: 核函数 
+    """
+    if kernel == "poly":
+        def poly_kernel(x1: np.ndarray, x2: np.ndarray) -> float:
+            """
+            多项式核函数
+            :param x1: 第一个向量
+            :param x2: 第二个向量
+            :return float: 两个向量的内积
+            """
+            return (1+np.dot(x1, x2))**kargs["degree"]
+        return poly_kernel
+
+    elif kernel == "linear":
+        def linear_kernel(x1: np.ndarray, x2: np.ndarray) -> float:
+            """
+            线性核函数
+            :param x1: 第一个向量
+            :param x2: 第二个向量
+            :return float: 两个向量的内积
+            """
+            return np.dot(x1, x2)
+        return linear_kernel
+
+    elif kernel == "rbf":
+        def gaussian_kernel(x1: np.ndarray, x2: np.ndarray) -> float:
+            """
+            高斯核函数
+            :param x1: 第一个向量
+            :param x2: 第二个向量
+            :return float: 两个向量的内积
+            """
+            return np.exp(-np.linalg.norm(x1-x2)**2 / (2 * (kargs["sigma"] ** 2)))
+        return gaussian_kernel
+
+    else:
+        raise ValueError("不支持的核函数")
+
+
+def nlsvm(X: np.ndarray, y: np.ndarray, cost:  float, kernel: Literal["linear", "poly", "rbf"], **kargs) -> list[Callable]:
     """
     模型训练的接口函数
     :param X: 训练数据的特征矩阵，n行，dim列，dim为数据维数，每一行为一个训练样本，每一列为一个特征
@@ -119,6 +141,7 @@ def nlsvm(X: np.ndarray, y: np.ndarray, cost:  float = 10.0, degree: int = 3) ->
     :param degree: 大于1的整数，多项式核函数的次数
     :return list: SVM多分类器
     """
+    kernelf: Callable = get_kernel(kernel, kargs)
     n, dim = X.shape
     nclass = len(np.unique(y))
     classes = ovo(y)  # 拆分成多个一对一元组
@@ -134,7 +157,7 @@ def nlsvm(X: np.ndarray, y: np.ndarray, cost:  float = 10.0, degree: int = 3) ->
         print(
             f"训练分类器: {i+1}/{len(classes)} ['{classes[i][0]}', '{classes[i][1]}']")
         model.append(nlsvm_solve(
-            X_tmp, y_tmp, (classes[i][0], classes[i][1]), cost, degree))
+            X_tmp, y_tmp, (classes[i][0], classes[i][1]), cost, kernelf))
     print("训练完成!")
     print(f"训练样本数: {n}；维数: {dim}；类别数: {nclass}")
     return model
@@ -170,8 +193,8 @@ def predict(model: list[Callable], X: np.ndarray) -> np.ndarray:
 def accuracy(y_pred: np.ndarray, y_test: np.ndarray) -> float:
     """
     计算测试样本的准确率
-    :param y_pred: 一维预测标签，类型为np.array(string)
-    :param y_test: 一维测试标签，类型为np.array(string)
+    :param y_pred: 一维预测标签，类型为np.ndarray[str]
+    :param y_test: 一维测试标签，类型为np.ndarray[str]
     :return float: 准确率
     """
     return sum(y_pred == y_test) / len(y_pred)
@@ -185,13 +208,14 @@ if __name__ == '__main__':
 
     from sklearn import datasets
     starttime = datetime.now()
-    print("对鸢尾花数据集进行训练......")
     lris_df = datasets.load_iris()
     X = lris_df.data
     y = lris_df.target.astype(int).astype(str)
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.4, random_state=42, stratify=y)
-    model = nlsvm(X_train, y_train, cost=10.0, degree=5)
+    # 高斯核函数，sigma=1.0
+    print("对鸢尾花数据集进行训练......")
+    model = nlsvm(X_train, y_train, cost=10.0, kernel="rbf", sigma=1.0)
     print("正在进行分类器测试......")
     y_predict = predict(model, X_test)
     print(f"测试集准确率: {accuracy(y_predict, y_test)*100:.2f}%")
@@ -210,7 +234,7 @@ if __name__ == '__main__':
 
     # starttime = datetime.now()
     # print("对手写数字数据集进行训练......")
-    # model = nlsvm(X_train, y_train, cost=1.0, degree=3)
+    # model = nlsvm(X_train, y_train, cost=1.0, kernel="poly", degree=3)
     # print("正在进行分类器测试......")
     # y_predict = predict(model, X_test)
     # print(f"测试集准确率: {accuracy(y_predict, y_test)*100:.2f}%\n")
